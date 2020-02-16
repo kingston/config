90536c17e2cb1b20695fc9c063691704aa54ca88
Kingston <kingston@users.noreply.github.com>
2020-02-16 23:21:10 +0100

    Add git pr shortcut for pushing to remote HEAD

--- setupGit.sh
+++ setupGit.sh
@@ -24,6 +24,7 @@ git config --global alias.br branch
 git config --global alias.hist "log --pretty=format:\"%h %ad | %s%d [%an]\" --graph --date=short"
 git config --global alias.type "cat-file -t"
 git config --global alias.dump "cat-file -p"
+git config --global alias.pr "push origin HEAD"
 
 git config --global core.editor "vim"
 
