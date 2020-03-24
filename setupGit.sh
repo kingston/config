f81df6a4dd3dfa8f77f96ada0fc5dbf22ef331bd
Kingston <web@ktam.org>
2020-03-24 09:49:05 -0700

    Add new git alias to undo commit

--- setupGit.sh
+++ setupGit.sh
@@ -29,6 +29,7 @@ git config --global alias.br branch
 git config --global alias.bd branch -d
 git config --global alias.bdm "branch --merged | grep -v '*' | xargs -n 1 git branch -d"
 git config --global alias.hist "log --pretty=format:\"%h %ad | %s%d [%an]\" --graph --date=short"
+git config --global alias.uc "reset --soft HEAD~1"
 git config --global alias.type "cat-file -t"
 git config --global alias.dump "cat-file -p"
 
