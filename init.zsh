19715db7a39880991d5bb3fc4773cdacf88759ed
Kingston Tam <web@ktam.org>
2025-06-17 10:57:35 +0200

    Add Aider ignore and init.zsh history settings

--- init.zsh
+++ init.zsh
@@ -23,34 +23,6 @@ fi
 
 zplug load
 
-#compdef pnpm
-###-begin-pnpm-completion-###
-if type compdef &>/dev/null; then
-  _pnpm_completion () {
-    local reply
-    local si=$IFS
-
-    IFS=$'\n' reply=($(COMP_CWORD="$((CURRENT-1))" COMP_LINE="$BUFFER" COMP_POINT="$CURSOR" SHELL=zsh pnpm completion-server -- "${words[@]}"))
-    IFS=$si
-
-    if [ "$reply" = "__tabtab_complete_files__" ]; then
-      _files
-    else
-      _describe 'values' reply
-    fi
-  }
-  # When called by the Zsh completion system, this will end with
-  # "loadautofunc" when initially autoloaded and "shfunc" later on, otherwise,
-  # the script was "eval"-ed so use "compdef" to register it with the
-  # completion system
-  if [[ $zsh_eval_context == *func ]]; then
-    _pnpm_completion "$@"
-  else
-    compdef _pnpm_completion pnpm
-  fi
-fi
-###-end-pnpm-completion-###
-
 bindkey '^S' autosuggest-accept
 
 if [[ "$(uname)" == "Darwin" ]]; then
@@ -58,3 +30,19 @@ if [[ "$(uname)" == "Darwin" ]]; then
 fi
 
 eval "$(~/.starship/starship init zsh)"
+
+# History settings
+
+setopt EXTENDED_HISTORY          # Write the history file in the ':start:elapsed;command' format.
+setopt INC_APPEND_HISTORY        # Write to the history file immediately, not when the shell exits.
+setopt SHARE_HISTORY             # Share history between all sessions.
+setopt HIST_EXPIRE_DUPS_FIRST    # Expire a duplicate event first when trimming history.
+setopt HIST_IGNORE_DUPS          # Do not record an event that was just recorded again.
+setopt HIST_IGNORE_ALL_DUPS      # Delete an old recorded event if a new event is a duplicate.
+setopt HIST_FIND_NO_DUPS         # Do not display a previously found event.
+setopt HIST_IGNORE_SPACE         # Do not record an event starting with a space.
+setopt HIST_SAVE_NO_DUPS         # Do not write a duplicate event to the history file.
+setopt HIST_VERIFY               # Do not execute immediately upon history expansion.
+setopt APPEND_HISTORY            # append to history file
+setopt HIST_NO_STORE             # Don't store history commands
+
