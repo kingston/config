9edeb1c42645c5b9e83c820821501b630d1be9a9
Kingston <web@ktam.org>
2025-09-09 13:25:13 +0200

    Try to fix path temporarily

--- setup.sh
+++ setup.sh
@@ -114,7 +114,7 @@ fi
 
 echo "Installing Vim Plug plugins..."
 
-vim -Es -V1~/vim-plug.log -u ~/.vimrc +PlugClean! +'PlugInstall --sync' +qa
+vim -Es -V1/home/vscode/vim-plug.log -u ~/.vimrc +PlugClean! +'PlugInstall --sync' +qa
 
 echo "Vim plug plugins successfully installed!"
 
